3774ebefeb91d0f39254ae5cff69fefbb295ebca
AbuJaan <sbilalulhassan@gmail.com>
2024-06-27 13:42:36 +0500

    Add files via upload

---
+++ Copyright.md
@@ -0,0 +1,8 @@
+## ©BilloXD 2024
+All rights reserved.
+
+The code, and content of this application are protected by international copyright laws. Unauthorized reproduction or distribution of this application or any portion of it may result in severe civil and criminal penalties and will be prosecuted to the maximum extent possible under the law.
+
+BilloXD retains all rights, including but not limited to the right to use, copy, distribute, and make derivative works of this application.
+
+
---
+++ index.js
@@ -0,0 +1,80 @@
+
+
+
+
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to Billo: http://localhost:${port}`);
+  console.log(`🔗 Powered By Billo`);
+});
+
+
+const statusMessages = ["Helping to BilloXD","Watching YouTube"];
+
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+
+
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨Follow ig.billo`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️Subscribe TO BilloXD`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+});
+
+login();
+
+
